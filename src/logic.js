f9cf12076f8ff54b7a2a8734710411e58edee302
Ebonie Butler <ebonie@metalandcoffee.com>
2022-02-24 16:06:05 -0500

    add test for adjacent food

--- src/logic.js
+++ src/logic.js
@@ -1,13 +1,13 @@
-const directions = ["up", "down", "left", "right"]
+const directions = ['up', 'down', 'left', 'right'];
 
 function info() {
-  console.log("INIT");
+  console.log('INIT');
   const response = {
-    apiversion: "1",
-    author: "",
-    color: "#4B0082",
-    head: "bendr",
-    tail: "freckled",
+    apiversion: '1',
+    author: '',
+    color: '#4B0082',
+    head: 'bendr',
+    tail: 'freckled',
   };
   return response;
 }
@@ -40,7 +40,7 @@ function move(gameState) {
     const baseDirections = {
       up: {
         x: snakeHead.x,
-        y: snakeHead.y + 1
+        y: snakeHead.y + 1,
       },
       down: {
         x: snakeHead.x,
@@ -53,11 +53,11 @@ function move(gameState) {
       right: {
         x: snakeHead.x + 1,
         y: snakeHead.y,
-      }
-    }
+      },
+    };
     const baseDirection = baseDirections[direction];
 
-    if (gameState.game.ruleset.name !== "wrapped") return baseDirection;
+    if (gameState.game.ruleset.name !== 'wrapped') return baseDirection;
 
     if (gameState.board.width === baseDirection.x) {
       baseDirection.x = 0;
@@ -70,7 +70,7 @@ function move(gameState) {
       baseDirection.y = gameState.board.height - 1;
     }
 
-    return baseDirection
+    return baseDirection;
   }
 
   /* Don't let your Battlesnake move back on its own neck */
@@ -86,43 +86,39 @@ function move(gameState) {
   //https://docs.battlesnake.com/references/api/sample-move-request
   /* Don't hit the walls */
   if ('wrapped' !== gameState.game.ruleset.name) {
-
     // If myHead.x is at the beginning of the x axis,
     // turn off "left" as possible move.
     if (myHead.x === 0) {
-      possibleMoves.left = false
+      possibleMoves.left = false;
       // If myHead.x is at the end of the x axis,
       // turn off "right" as possible move.
     } else if (myHead.x === gameState.board.width - 1) {
-      possibleMoves.right = false
+      possibleMoves.right = false;
     }
     // If myHead.y is at the beginning of the y axis,
     // turn off "down" as possible move.
     if (myHead.y === 0) {
-      possibleMoves.down = false
+      possibleMoves.down = false;
       // If myHead.y is at the end of the y axis,
       // turn off "up" as possible move.
     } else if (myHead.y === gameState.board.height - 1) {
-      possibleMoves.up = false
+      possibleMoves.up = false;
     }
   }
-
 
   /* Don't hit yourself or others. */
   // Use information in gameState to prevent your Battlesnake from colliding with itself.
 
   for (const snake of gameState.board.snakes) {
     for (let i = 0; i < snake.length - 1; i++) {
-
       for (const direction of directions) {
         if (
           //  {x:0,y:0} {y:0,x:0} properties aren't always in the same order.. update
-          JSON.stringify(snake.body[i]) === JSON.stringify(moveLookAhead(myHead, direction))
+          JSON.stringify(snake.body[i]) ===
+          JSON.stringify(moveLookAhead(myHead, direction))
         ) {
           possibleMoves[direction] = false;
         }
-
-
       }
     }
   }
@@ -130,15 +126,21 @@ function move(gameState) {
   /* Avoid loser snake heads that are adjacent to possible moves. */
   // If losersnake's head is 1 distance away from one of my possible moves, disable move.
   // @todo update logic to check if I am stronger than snake first before avoiding.
-  let loserSnakes = gameState.board.snakes.filter(snake => snake.id !== gameState.you.id).map((snake) => snake.head);
+  let loserSnakes = gameState.board.snakes
+    .filter((snake) => snake.id !== gameState.you.id)
+    .map((snake) => snake.head);
   for (let i = 0; i < loserSnakes.length; i++) {
     for (const direction of directions) {
       // Find distance from possible move to losersnake's head.
-      let xDistFrom = Math.abs(moveLookAhead(myHead, direction).x - loserSnakes[i].x);
+      let xDistFrom = Math.abs(
+        moveLookAhead(myHead, direction).x - loserSnakes[i].x
+      );
       if (xDistFrom > gameState.board.width / 2) {
         xDistFrom = gameState.board.width - xDistFrom;
       }
-      let yDistFrom = Math.abs(moveLookAhead(myHead, direction).y - loserSnakes[i].y);
+      let yDistFrom = Math.abs(
+        moveLookAhead(myHead, direction).y - loserSnakes[i].y
+      );
       if (yDistFrom > gameState.board.height / 2) {
         yDistFrom = gameState.board.height - yDistFrom;
       }
@@ -150,7 +152,6 @@ function move(gameState) {
     }
   }
 
-
   /* Avoid hazard sauce. */
   if (numberOfEnabledMoves() > 1) {
     const hazards = gameState.board.hazards;
@@ -158,7 +159,7 @@ function move(gameState) {
       for (const direction of directions) {
         if (
           JSON.stringify(hazards[i]) ===
-          JSON.stringify(moveLookAhead(myHead, direction)) &&
+            JSON.stringify(moveLookAhead(myHead, direction)) &&
           numberOfEnabledMoves() > 1
         ) {
           possibleMoves[direction] = false;
@@ -186,10 +187,6 @@ function move(gameState) {
       });
     }
 
-    // if (safeFood.length !== 0) {
-    //   food = safeFood;
-    // }
-
     // Sort all food from closest to furthest.
     food.sort((a, b) => {
       // Find total steps needed from myHead to aFood and to bFood. This is logic specifically trailored for Wrapped mode.
@@ -214,9 +211,6 @@ function move(gameState) {
         yDistFromB = gameState.board.height - yDistFromB;
       }
 
-      //tmp[JSON.stringify(a)] = { x: xDistFromA, y: yDistFromA };
-      //tmp[JSON.stringify(b)] = { x: xDistFromB, y: yDistFromB };
-
       const aDistanceToMe = xDistFromA + yDistFromA;
       const bDistanceToMe = xDistFromB + yDistFromB;
       return aDistanceToMe - bDistanceToMe;
@@ -226,14 +220,25 @@ function move(gameState) {
     const closestFood = food[0];
     const leftOrRight = myHead.x - closestFood.x;
     const topOrBottom = myHead.y - closestFood.y;
-    if (leftOrRight > 0) {
+
+    if (leftOrRight === 0) {
+      possibleMoves.right = false;
+      if (numberOfEnabledMoves() > 1) {
+        possibleMoves.left = false;
+      }
+    } else if (leftOrRight > 0) {
       possibleMoves.right = false;
     } else if (leftOrRight < 0) {
       possibleMoves.left = false;
     }
 
     if (numberOfEnabledMoves() > 1) {
-      if (topOrBottom > 0) {
+      if (topOrBottom === 0) {
+        possibleMoves.up = false;
+        if (numberOfEnabledMoves() > 1) {
+          possibleMoves.down = false;
+        }
+      } else if (topOrBottom > 0) {
         possibleMoves.up = false;
       } else if (topOrBottom < 0) {
         possibleMoves.down = false;
